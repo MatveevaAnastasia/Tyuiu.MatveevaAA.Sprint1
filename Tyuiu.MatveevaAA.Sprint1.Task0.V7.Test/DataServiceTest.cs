ff24e23b56b201e7f49fb590ac39beb86f1cee34
Anastasia Matveeva <kekkekyshk@gmail.com>
2025-09-28 19:44:47 +0500

    Task0

---
+++ Tyuiu.MatveevaAA.Sprint1.Task0.V7.Test/DataServiceTest.cs
@@ -0,0 +1,17 @@
+﻿
+using Tyuiu.MatveevaAA.Sprint1.Task0.V7.Lib;
+
+namespace Tyuiu.MatveevaAA.Sprint1.Task0.V7.Test
+{
+    [TestClass]
+    public sealed class DataServiceTest
+    {
+        [TestMethod]
+        public void ValidExspenssion()
+        {
+            DataService ds = new DataService();
+            var res = ds.Calculate();
+            Assert.AreEqual(5, res);
+        }
+    }
+}
