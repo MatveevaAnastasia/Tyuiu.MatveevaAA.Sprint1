9943fd7bb4df53033fe5a104428bae801cb98930
Anastasia Matveeva <kekkekyshk@gmail.com>
2025-09-28 22:30:47 +0500

    Task2

--- Tyuiu.MatveevaAA.Sprint1.Task2.V1.Test/DataServiceTest.cs
+++ Tyuiu.MatveevaAA.Sprint1.Task2.V1.Test/DataServiceTest.cs
@@ -11,7 +11,7 @@ namespace Tyuiu.MatveevaAA.Sprint1.Task2.V1.Test
             DataService ds = new DataService();
             int x = 1609;
             var res = ds.ConvertKmToM(x);
-            Assert.AreEqual(1000, res);
+            Assert.AreEqual(2588.881, res);
         }
     }
 }
