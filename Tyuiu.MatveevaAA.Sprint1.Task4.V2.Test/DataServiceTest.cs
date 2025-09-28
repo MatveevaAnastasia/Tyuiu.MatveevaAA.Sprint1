8216278787d1ef5fdc11f7032f8cf8f99d7d1c41
Anastasia Matveeva <kekkekyshk@gmail.com>
2025-09-29 03:33:49 +0500

    Task5

---
+++ Tyuiu.MatveevaAA.Sprint1.Task4.V2.Test/DataServiceTest.cs
@@ -0,0 +1,19 @@
+﻿using Tyuiu.MatveevaAA.Sprint1.Task4.V2.Lib;
+
+namespace Tyuiu.MatveevaAA.Sprint1.Task4.V2.Test
+{
+    [TestClass]
+    public sealed class DataServiceTest
+    {
+        [TestMethod]
+        public void ValidExpersiion()
+        {
+            DataService ds = new DataService();
+            double x = 5;
+            double y = 10;
+            double wait = 0.2;
+            var res = ds.Calculate(x, y);
+            Assert.AreEqual(wait, res);
+        }
+    }
+}
---
+++ Tyuiu.MatveevaAA.Sprint1.Task4.V2/Program.cs
@@ -0,0 +1,30 @@
+﻿using Tyuiu.MatveevaAA.Sprint1.Task4.V2.Lib;
+
+internal class Program
+{
+    private static void Main(string[] args)
+    {
+        DataService ds = new DataService();
+
+        Console.WriteLine("***************************************************************************");
+        Console.WriteLine("* ИСХОДНЫЕ ДАННЫЕ:                                                        *");
+        Console.WriteLine("***************************************************************************");
+
+        int x, y;
+
+        Console.WriteLine("Введите значение X: ");
+        x = Convert.ToInt32(Console.ReadLine());
+        Console.WriteLine("Введите значение Y: ");
+        y = Convert.ToInt32(Console.ReadLine());
+
+        Console.WriteLine("***************************************************************************");
+        Console.WriteLine("* РЕЗУЛЬТАТ:                                                              *");
+        Console.WriteLine("***************************************************************************");
+
+        Console.WriteLine(" 1 / sqrt( x + y * 2) = " + ds.Calculate(x, y));
+
+        Console.ReadKey();
+
+
+    }
+}
